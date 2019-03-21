f0ac9c51c37f764ad750ad4782573ec8a9513aa2
Steve Hipwell <steve.hipwell@gmail.com>
2019-03-21 12:24:04 +0000

    Remove header

--- src/GeoTiffNet/TiffBase.cs
+++ src/GeoTiffNet/TiffBase.cs
@@ -1,15 +1,19 @@
 using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace GeoTiffNet
 {
   public abstract class TiffBase : IDisposable
   {
+    private const int HeaderByteCount = 8;
+
+    protected IByteHandler ByteHandler;
     protected readonly Stream Stream;
     private readonly bool OwnsStream;
-    protected readonly ITiffHeader Header;
     private bool ImagesLoaded;
     private IList<TiffImage> Images;
 
@@ -17,14 +21,24 @@ namespace GeoTiffNet
     {
       this.Stream = stream;
       this.OwnsStream = ownsStream;
-      this.Header = new TiffHeader(this.Stream, isGeoTiff);
+      this.IsGeoTiff = isGeoTiff;
       this.ImagesLoaded = false;
+
+      this.Load();
     }
 
     ~TiffBase()
     {
       this.Dispose(false);
     }
+
+    public TiffByteOrderEnum ByteOrder { get; private set; }
+
+    public int VersionNumber { get; private set; }
+
+    public bool IsGeoTiff { get; private set; }
+
+    public long FirstImageOffset { get; private set; }
 
     protected IList<TiffImage> GetImagesBase()
     {
@@ -53,14 +67,26 @@ namespace GeoTiffNet
       }
     }
 
+    private void Load()
+    {
+      this.Stream.Position = 0;
+      using (var reader = new BinaryReader(this.Stream, Encoding.UTF8, true))
+      {
+        this.ByteOrder = (TiffByteOrderEnum)BinaryPrimitives.ReadInt16LittleEndian(reader.ReadBytes(2));
+        this.ByteHandler = new ByteHandler(this.ByteOrder == TiffByteOrderEnum.BigEndian);
+        this.VersionNumber = this.ByteHandler.ReadUInt16(reader.ReadBytes(2));
+        this.FirstImageOffset = this.ByteHandler.ReadUInt32(reader.ReadBytes(4));
+      }
+    }
+
     private void LoadImages()
     {
       this.Images = new List<TiffImage>();
 
-      long imageOffset = this.Header.FirstImageOffset;
+      long imageOffset = this.FirstImageOffset;
       while (imageOffset != 0)
       {
-        var image = new TiffImage(this.Header.ByteHandler, this.Stream, imageOffset, this.Header.IsGeoTiff);
+        var image = new TiffImage(this.ByteHandler, this.Stream, imageOffset, this.IsGeoTiff);
         this.Images.Add(image);
 
         imageOffset = image.NextImageOffset;
